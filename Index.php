8569eb8a5eeb8ab9d612a42676ed5e1c5609083b
Bosi Chen <chenbosi.bosi@gmail.com>
2023-11-20 00:41:51 -0500

    add landing page

---
+++ Index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+?>
+<?php include_once './src/Header.php'; ?>
+
+<main class="container p-5">
+    <h1 class="text-center">Welcome to Algonquin Social Media Website</h1>
+    <div class="my-3">
+        <p>If you have never used this before, you have to <a href="NewUser.php">sign up</a> first.</p>
+        <p>If you have already signed up, you can <a href="Login.php">log in</a> now</p>
+    </div>
+</main>
+
+<?php include_once './src/Footer.php'; ?>
