15b6ba611e585024c6c855905fb98d11fa9157b2
Bosi Chen <chenbosi.bosi@gmail.com>
2023-11-20 00:28:44 -0500

    add common footer

---
+++ src/Footer.php
@@ -0,0 +1,9 @@
+    <footer style="position: fixed; bottom: 0; width: 100vw; height: 60px; background-color: darkgreen;">
+      <div class="container">
+        <p style="text-align: center; padding: 10px; color: white;">&copy; Algonquin College 2010 - <?php date_default_timezone_set("America/Toronto"); print Date("Y"); ?>. All Rights Reserved</p>
+      </div>
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+</body>
+</html> 
